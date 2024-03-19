8972c84275c91c07f4fb73e2988cebfd5f51d3dd
Stamboliysky <jordanspasssoff@gmail.com>
2024-03-19 17:41:39 +0100

    Añadir ClassName bg-color Padre,Hijo flex Justfy-between

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline
-    bg-red-200">Hello world!</h1>
+    <h1>Hello world!
+    <div className="bg-blue-200 flex justify-between">Soy el Padre 
+    <div className="bg-red-100 w-fit"> Soy el Hijo </div>
+     </div>
+    </h1>
   )
 }
